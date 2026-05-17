ea6500fff577f8583cb636144d345f1c40729049
decembermcnabb <decembermcnabb@gmail.com>
2026-05-16 22:36:31 -0700

    Hour 14 close to finished

---
+++ dmcnabb_hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,20 @@
+/* 
+imported background image: Fullmetal Alchemist, converted from WebP file type to PNG, then converted to Sprite in Unity. Found on google image search. 
+    
+colours: Colors are gray, black, red, and variants of each. 
+    
+typeface: What's type face?
+    
+font sizes/styles (change to new font(s)): 39.8 Bold, 30 Bold. Added underlay. 
+    
+optional: Play around with colour gradients! Links to an external site: WELL... I tried to play around inside Unity to achieve that, but no such luck. 
+
+Modify the button colours and transitions: I'm not sure if this is different from "colours" section. It's the same to me as listed above. 
+
+Add some custom text specific to your game!: I did! It's called Wolfred, which it seems is the closest type to the Fullmetal Alchemist font style. 
+Game/interface title: This was already provided for me in the picture, so I didn't want to mess with it. 
+    
+Game instructions or some sort of description: Hm... I'll add the screens after clicking Start, Load Game, Options. Start will turn into an instructions to play screen, but the font
+is very restrictve to colons and other symbols. &sad face&
+*/
+
