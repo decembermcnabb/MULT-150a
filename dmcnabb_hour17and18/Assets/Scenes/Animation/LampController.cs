1d65ab2636b1facd63bdb8f1f5bc77b7603028d9
decembermcnabb <decembermcnabb@gmail.com>
2026-05-24 10:48:13 -0700

    hour 17 finished, ethan unfinished
    Hour 17 had a simple solution I overlooked/forgot. Just add a plane for the light to bounce off for Game View visibility. The other part was to turn up the intensity as it was pretty low.

---
+++ dmcnabb_hour17and18/Assets/Scenes/Animation/LampController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LampController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("=== W key pressed - calling SetTrigger('Recolour') ===");
+            animator.SetTrigger("Recolour");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+            animator.SetTrigger("Scale");
+
+        if (Input.GetKeyDown(KeyCode.S))
+            animator.SetTrigger("Rotate");
+
+        if (Input.GetKeyDown(KeyCode.D))
+            animator.SetTrigger("Float");
+    }
+}
